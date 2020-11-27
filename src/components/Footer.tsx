2afb98aa8f50b857830cf2f500ed988c62695644
lmjcbs <liam@lmjcbs.com>
2020-11-27 18:08:01 +0000

    restyle footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,20 @@
+import Link from 'next/link'
+import { TwitterIcon } from '../lib/svg'
+
 const Footer = () => {
   return (
     <footer>
-      <div className="mx-auto max-w-6xl flex flex-row items-center mt-16 p-6 justify-between bg-gray-100">
-        <p className="px-6 text-sm">2020 Remoteja</p>
-        <p className="px-6 text-sm">Navigation</p>
-        <p className="px-6 text-sm">Links</p>
+      <div className="mx-auto max-w-6xl flex flex-row items-center mt-16 p-6 justify-between">
+        <p className="px-6 text-sm font-semibold text-gray-800">
+          2020 Remoteja
+        </p>
+        <p className="px-6 text-sm">
+          <Link href="https://twitter.com/remoteja">
+            <a target="_blank" rel="noopener">
+              <TwitterIcon />
+            </a>
+          </Link>
+        </p>
       </div>
     </footer>
   )
