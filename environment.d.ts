582710740d400638ab60dfe48aff4a9099905c1f
lmjcbs <liam@lmjcbs.com>
2021-01-12 21:21:41 +0000

    add twitter api key

--- environment.d.ts
+++ environment.d.ts
@@ -1,8 +1,11 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      GITHUB_ID: string
-      GITHUB_SECRET: string
+      TWITTER_API_KEY: string
+      TWITTER_API_SECRET: string
+      TWITTER_BEARER_TOKEN: string
+      TWITTER_ACCESS_TOKEN_KEY: string
+      TWITTER_ACCESS_TOKEN_KEY_SECRET: string
       SMTP_HOST: string
       SMTP_PORT: number
       SMTP_USER: string
