34e22ac83aa6c0224622b3e52e5cf7572dd9f884
lmjcbs <liam@lmjcbs.com>
2021-01-13 18:02:14 +0000

    dynamically change title on job page

--- src/pages/remote-jobs/[slug].tsx
+++ src/pages/remote-jobs/[slug].tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { ParsedUrlQuery } from 'querystring'
 import Link from 'next/link'
+import Head from 'next/head'
 import { useRouter } from 'next/router'
 import { PrismaClient } from '@prisma/client'
 import { JobPreviewTile } from '../../components'
@@ -36,114 +37,123 @@ const Job: FC<JobsProps> = ({ job, relatedJobs, twitterShareLink }) => {
   }
 
   return (
-    <main className="px-1 md:px-2 lg:px-4">
-      <div className="flex flex-row justify-between mt-6">
-        <Link href="/">
-          <div className="flex flex-row items-center space-x-2 cursor-pointer">
-            <ArrowLeftIcon size={20} className="fill-current text-gray-700" />
-            <a className="text-left text-base font-medium">All Remote Jobs</a>
-          </div>
-        </Link>
-
-        <Link href={`/categories/${job.category.name}`}>
-          <div className="flex flex-row items-center space-x-2 cursor-pointer">
-            <a className="text-right capitalize text-base font-medium">
-              More Remote {job.category.name} Jobs
-            </a>
-            <ArrowRightIcon size={20} className="fill-current text-gray-700" />
-          </div>
-        </Link>
-      </div>
-
-      <div className="text-gray-800 font-semibold pt-6 text-sm md:text-base">
-        Posted {job.datePosted}
-      </div>
-
-      <div className="pt-4">
-        <h2 className="text-xl lg:text-2xl font-semibold text-gray-700">
-          {job.companyName}
-        </h2>
-      </div>
-
-      <div>
-        <h1 className="text-2xl lg:text-3xl font-bold text-gray-800">
-          {job.title}
-        </h1>
-      </div>
-
-      <div className="mt-2 space-x-1 lg:space-x-2">
-        {job.tags.map(({ name, id }) => (
-          <Link key={id} href={`/tags/${name}`}>
-            <a className="inline-block bg-indigo-200 hover:bg-indigo-300 border-2 hover:border-indigo-300 border-indigo-200 rounded-md px-1 md:px-2 py-0 text-sm font-medium text-gray-700 shadow-md tracking-wide">
-              <span className="font-bold">#</span>
-              {name}
-            </a>
-          </Link>
-        ))}
-      </div>
-
-      <div className="flex flex-row items-center space-x-1 my-3">
-        <MapMarkerIcon size={18} className="fill-current text-gray-600" />
-        <Link href={`/locations/${job.location.name.replace(' ', '-')}`}>
-          <a className="text-base capitalize font-medium text-gray-800 hover:underline">
-            {job.location.name}
-          </a>
-        </Link>
-
-        <div className="mx-1 text-base">·</div>
-        <Link href={`/categories/${job.category.name.replace(' ', '-')}`}>
-          <a className="text-base capitalize font-medium text-gray-800 hover:underline">
-            {job.category.name}
-          </a>
-        </Link>
-      </div>
-
-      <div className="border-b-2 py-1"></div>
-
-      <h3 className="font-semibold text-xl mb-2 mt-6">About the Role</h3>
-
-      <div
-        id="description"
-        className="space-y-3 lg:space-y-4 text-gray-800"
-        dangerouslySetInnerHTML={{ __html: job.descriptionAsHTML }}
-      ></div>
-
-      <div className="flex flex-row items-center justify-between my-6">
-        <Link href={`${job.applyUrl}?ref=remoteja.com`}>
-          <a target="_blank" rel="noopener">
-            <div className="px-2 py-1 bg-indigo-500 rounded-md text-white font-semibold hover:bg-indigo-600 shadow-lg">
-              Apply For This Position
+    <>
+      <Head>
+        <title>Remote {job.title}</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <main className="px-1 md:px-2 lg:px-4">
+        <div className="flex flex-row justify-between mt-6">
+          <Link href="/">
+            <div className="flex flex-row items-center space-x-2 cursor-pointer">
+              <ArrowLeftIcon size={20} className="fill-current text-gray-700" />
+              <a className="text-left text-base font-medium">All Remote Jobs</a>
             </div>
-          </a>
-        </Link>
-        <div className="flex flex-row items-center space-x-2 mr-4 lg:mr-8">
-          <p className="block text-base lg:text-lg font-semibold text-gray-700">
-            Share
-          </p>
-          <div className="flex flex-row space-x-2 w-full m-auto">
-            <Link href={getJobMailtoLink(job)}>
-              <a rel="noopener">
-                <MailtoIcon size={24} />
-              </a>
-            </Link>
-            <Link href={twitterShareLink}>
-              <a target="_blank" rel="noopener">
-                <TwitterIcon size={24} />
-              </a>
-            </Link>
-          </div>
-        </div>
-      </div>
+          </Link>
 
-      <div className="mt-12">
-        <h4 className="text-xl font-semibold text-gray-700">Similar Jobs</h4>
+          <Link href={`/categories/${job.category.name}`}>
+            <div className="flex flex-row items-center space-x-2 cursor-pointer">
+              <a className="text-right capitalize text-base font-medium">
+                More Remote {job.category.name} Jobs
+              </a>
+              <ArrowRightIcon
+                size={20}
+                className="fill-current text-gray-700"
+              />
+            </div>
+          </Link>
+        </div>
+
+        <div className="text-gray-800 font-semibold pt-6 text-sm md:text-base">
+          Posted {job.datePosted}
+        </div>
+
+        <div className="pt-4">
+          <h2 className="text-xl lg:text-2xl font-semibold text-gray-700">
+            {job.companyName}
+          </h2>
+        </div>
+
         <div>
-          {relatedJobs.map((job) => (
-            <JobPreviewTile job={job}></JobPreviewTile>
+          <h1 className="text-2xl lg:text-3xl font-bold text-gray-800">
+            {job.title}
+          </h1>
+        </div>
+
+        <div className="mt-2 space-x-1 lg:space-x-2">
+          {job.tags.map(({ name, id }) => (
+            <Link key={id} href={`/tags/${name}`}>
+              <a className="inline-block bg-indigo-200 hover:bg-indigo-300 border-2 hover:border-indigo-300 border-indigo-200 rounded-md px-1 md:px-2 py-0 text-sm font-medium text-gray-700 shadow-md tracking-wide">
+                <span className="font-bold">#</span>
+                {name}
+              </a>
+            </Link>
           ))}
         </div>
-      </div>
-    </main>
+
+        <div className="flex flex-row items-center space-x-1 my-3">
+          <MapMarkerIcon size={18} className="fill-current text-gray-600" />
+          <Link href={`/locations/${job.location.name.replace(' ', '-')}`}>
+            <a className="text-base capitalize font-medium text-gray-800 hover:underline">
+              {job.location.name}
+            </a>
+          </Link>
+
+          <div className="mx-1 text-base">·</div>
+          <Link href={`/categories/${job.category.name.replace(' ', '-')}`}>
+            <a className="text-base capitalize font-medium text-gray-800 hover:underline">
+              {job.category.name}
+            </a>
+          </Link>
+        </div>
+
+        <div className="border-b-2 py-1"></div>
+
+        <h3 className="font-semibold text-xl mb-2 mt-6">About the Role</h3>
+
+        <div
+          id="description"
+          className="space-y-3 lg:space-y-4 text-gray-800"
+          dangerouslySetInnerHTML={{ __html: job.descriptionAsHTML }}
+        ></div>
+
+        <div className="flex flex-row items-center justify-between my-6">
+          <Link href={`${job.applyUrl}?ref=remoteja.com`}>
+            <a target="_blank" rel="noopener">
+              <div className="px-2 py-1 bg-indigo-500 rounded-md text-white font-semibold hover:bg-indigo-600 shadow-lg">
+                Apply For This Position
+              </div>
+            </a>
+          </Link>
+          <div className="flex flex-row items-center space-x-2 mr-4 lg:mr-8">
+            <p className="block text-base lg:text-lg font-semibold text-gray-700">
+              Share
+            </p>
+            <div className="flex flex-row space-x-2 w-full m-auto">
+              <Link href={getJobMailtoLink(job)}>
+                <a rel="noopener">
+                  <MailtoIcon size={24} />
+                </a>
+              </Link>
+              <Link href={twitterShareLink}>
+                <a target="_blank" rel="noopener">
+                  <TwitterIcon size={24} />
+                </a>
+              </Link>
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-12">
+          <h4 className="text-xl font-semibold text-gray-700">Similar Jobs</h4>
+          <div>
+            {relatedJobs.map((job) => (
+              <JobPreviewTile job={job}></JobPreviewTile>
+            ))}
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
 
