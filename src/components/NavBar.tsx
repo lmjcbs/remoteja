fd9f8f7c320982be2c54d95e73752b7c8c2c3de0
lmjcbs <liam@lmjcbs.com>
2021-01-14 19:05:29 +0000

    add mobile nav icon

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,18 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 import Link from 'next/link'
 import CategoriesDropdown from './CategoriesDropdown'
 
 const NavBar: FC = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const handleMenuClick = (): void => {
+    setIsOpen(!isOpen)
+  }
+
   return (
-    <nav>
-      <div className="mx-auto max-w-6xl flex flex-row items-center py-4 justify-between">
-        <div className="ml-3 md:ml-8">
+    <header>
+      <div className="flex items-center justify-between px-4 py-3">
+        <div className="">
           <Link href="/">
             <a className="text-indigo-500 font-medium text-xl">
               remote
@@ -16,14 +22,32 @@ const NavBar: FC = () => {
             </a>
           </Link>
         </div>
-        <div className="flex flex-row items-center">
-          <CategoriesDropdown />
-          {/* <div className="mx-4 bg-indigo-500 hover:bg-indigo-600 text-white rounded-md py-1 px-2 font-semibold shadow-lg">
-            Post a Job
-          </div> */}
-        </div>
+        <button
+          type="button"
+          className="text-gray-800 hover:text-indigo-600  focus:outline-none"
+        >
+          <svg
+            onClick={handleMenuClick}
+            className="h-8 w-8 fill-current block"
+            viewBox="0 0 24 24"
+          >
+            {isOpen ? (
+              // Open menu state
+              <path d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z" />
+            ) : (
+              // Closed menu state
+              <path d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z" />
+            )}
+          </svg>
+          {/* <CategoriesDropdown /> */}
+        </button>
       </div>
-    </nav>
+      <div className={`px-4 pt-2 pb-4 ${isOpen ? 'block' : 'hidden'}`}>
+        {/* <button className="block text-gray-900 font font-semibold">
+          Categories
+        </button> */}
+      </div>
+    </header>
   )
 }
 
