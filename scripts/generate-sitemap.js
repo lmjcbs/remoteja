fa87b743e8f63bf0ce5433a21e18966532279eda
lmjcbs <liam@lmjcbs.com>
2021-01-24 21:43:25 +0000

    update sitemap config

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -35,7 +35,7 @@ const generateSitemap = async () => {
         <?xml version="1.0" encoding="UTF-8"?>
         <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
           <url>
-            <loc>https://remoteja.com</loc>
+            <loc>https://remoteja.com/</loc>
             <changefreq>hourly</changefreq>
             <priority>1.0</priority>
           </url>
