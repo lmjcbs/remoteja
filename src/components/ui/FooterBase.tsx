a2482820bb2ace84d34028d08caa00a42bc03d45
lmjcbs <liam@lmjcbs.com>
2021-01-25 12:04:55 +0000

    refactor ui to chakra

--- src/components/ui/FooterBase.tsx
+++ src/components/ui/FooterBase.tsx
@@ -6,13 +6,13 @@ import { TwitterIcon } from '../../lib/svg'
 const FooterBase: FC = () => {
   return (
     <div className="flex-grow w-full mx-auto justify-center max-w-5xl">
-      <Flex px={8} pb={8} align="center" justify="space-between">
+      <Flex px={4} pb={8} align="center" justify="space-between">
         <Heading as="h4" fontSize="md">
           Remoteja <Text as="span">2021</Text>
         </Heading>
         <Link href="https://twitter.com/remoteja">
           <a target="_blank" rel="noopener">
-            <TwitterIcon size={20} />
+            <TwitterIcon size={6} />
           </a>
         </Link>
       </Flex>
