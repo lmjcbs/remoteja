ec04b96a349d467314b28ad5341e397c70e572c9
lmjcbs <liam@lmjcbs.com>
2021-01-27 12:21:59 +0000

    add product, design

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,12 @@
 export const ONE_DAY_EPOCH = 86400
 export const ONE_MONTH_EPOCH = 2592000
 
-export const categories = ['developer', 'sales', 'marketing', 'finance']
+export const categories = [
+  'developer',
+  'sales',
+  'marketing',
+  'finance',
+  'design',
+  'product',
+]
 export const locations = ['worldwide', 'north america', 'europe']
