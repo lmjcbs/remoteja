3047fb368524778b70bc904356ede4e099ff5a41
lmjcbs <liam@lmjcbs.com>
2021-01-31 20:53:14 +0000

    refactor tags page

--- src/pages/tags/[name].tsx
+++ src/pages/tags/[name].tsx
@@ -1,37 +1,37 @@
-import { FC } from 'react'
 import Head from 'next/head'
 import Header from '../../components/sections/Header'
+import prisma from '../../lib/prisma'
+import { dateStripped, getUrlSlug } from '../../utils'
+import { JobPreviewTile } from '../../components'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { ParsedUrlQuery } from 'querystring'
-import { PrismaClient } from '@prisma/client'
-import { extendJobsData, capitalize } from '../../utils'
-import { JobPreviewTile } from '../../components'
 import { useRouter } from 'next/router'
 
-type Props = {
-  jobs: Models.JobWithRelations[]
+type TagProps = {
+  jobs: JobWithRelations[]
   tag: string
 }
 
-const Tags: FC<Props> = ({ jobs, tag }) => {
-  const router = useRouter()
+const Tags = ({ jobs, tag }: TagProps) => {
+  const { isFallback } = useRouter()
 
-  if (router.isFallback) {
-    return <div>Loading</div>
+  if (isFallback) {
+    return <div>Loading...</div>
   }
+
   return (
     <main>
       <Head>
-        <title>Remote {capitalize(tag)} Jobs | Remoteja</title>
+        <title>Remote {tag} Jobs | Remoteja</title>
         <meta
           property="title"
           key="title"
-          content={`Remote ${capitalize(tag)} Jobs | Remoteja`}
+          content={`Remote ${tag} Jobs | Remoteja`}
         />
         <meta
           property="og:title"
           key="og:title"
-          content={`Remote ${capitalize(tag)} Jobs | Remoteja`}
+          content={`Remote ${tag} Jobs | Remoteja`}
         />
         <meta
           property="description"
@@ -45,7 +45,7 @@ const Tags: FC<Props> = ({ jobs, tag }) => {
         />
       </Head>
       <Header
-        h1={`Remote ${capitalize(tag)} Jobs`}
+        h1={`Remote ${tag} Jobs`}
         h2={`Looking for remote ${tag} jobs? View the lastest job listings from
           companies hiring for ${tag} positions.`}
       />
@@ -57,8 +57,6 @@ const Tags: FC<Props> = ({ jobs, tag }) => {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  const prisma = new PrismaClient()
-
   const tags = await prisma.tag.findMany()
 
   const paths = tags.map(({ slug }) => {
@@ -68,8 +66,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
       },
     }
   })
-
-  await prisma.$disconnect()
 
   return {
     paths,
@@ -81,31 +77,56 @@ interface Params extends ParsedUrlQuery {
   name: string
 }
 
-export const getStaticProps: GetStaticProps<Props, Params> = async (
+export const getStaticProps: GetStaticProps<TagProps, Params> = async (
   context
 ) => {
-  const params = context.params as Params
-  const prisma = new PrismaClient()
+  const params: Params = context.params as Params
 
-  const rawData = await prisma.job.findMany({
+  const tag = await prisma.tag.findUnique({
+    where: { slug: params.name },
+    rejectOnNotFound: true,
+  })
+
+  if (!tag) return { notFound: true }
+
+  const data = await prisma.job.findMany({
+    select: {
+      jid: true,
+      applyCTA: true,
+      createdEpoch: true,
+      companyName: true,
+      datePosted: true,
+      descriptionAsHTML: true,
+      featured: true,
+      salaryCurrency: true,
+      salaryMin: true,
+      salaryMax: true,
+      title: true,
+      location: true,
+      category: true,
+      tags: true,
+      type: true,
+    },
     where: { tags: { some: { slug: { contains: params.name } } } },
-    include: { location: true, category: true, tags: true, type: true },
     orderBy: [{ featured: 'desc' }, { createdEpoch: 'desc' }],
   })
 
-  // getStaticProps Fails to Serialize Date Object
-  const stringifiedData = JSON.stringify(rawData)
-  const data = JSON.parse(stringifiedData)
+  const jobs = data.map((job) => {
+    return dateStripped({
+      ...job,
+      daysSinceEpoch: Math.floor(
+        (Math.round(Date.now() / 1000) - job.createdEpoch) / 86400
+      ),
+      urlSlug: getUrlSlug(job),
+    }) as JobWithRelations
+  })
 
-  // Adds .daysSinceEpoch & .urlSlug
-  const extendedJobsData = extendJobsData(data)
-
-  await prisma.$disconnect()
+  await prisma.$disconnect
 
   return {
     props: {
-      jobs: extendedJobsData,
-      tag: params.name,
+      jobs: jobs,
+      tag: tag.name,
     },
     // Attempt to re-generate page on request at most once every second
     revalidate: 1,
