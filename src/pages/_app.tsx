ce45fe1e96260316143a97c8c869e1cccaeb0377
lmjcbs <liam@lmjcbs.com>
2021-02-07 12:27:29 +0000

    update site base meta

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -22,7 +22,20 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
       <Head>
+        {/* Primary Meta Tags */}
         <meta charSet="utf-8" key="charSet" />
+        <title>
+          Remoteja | Remote Jobs in Programming, Sales, Marketing, Design and
+          more
+        </title>
+        <meta
+          name="title"
+          content="Remoteja | Remote Jobs in Programming, Sales, Marketing, Design and more"
+        />
+        <meta
+          name="description"
+          content="Say yes to remote working and start hiring the best talent from all corners of the world today. Remoteja has the lastest listings for remote jobs in Programming, Sales, Maketing, Design and more."
+        />
         <meta
           name="viewport"
           content="initial-scale=1.0, width=device-width"
@@ -30,6 +43,35 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         />
         <meta property="og:site_name" content="Remoteja" />
         <link rel="icon" type="image/x-icon" href="favicon.ico" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://remoteja.com/" />
+        <meta
+          property="og:title"
+          content="Remoteja | Remote Jobs in Programming, Sales, Marketing, Design and more"
+        />
+        <meta
+          property="og:description"
+          content="Say yes to remote working and start hiring the best talent from all corners of the world today. Remoteja has the lastest listings for remote jobs in Programming, Sales, Maketing, Design and more."
+        />
+        <meta property="og:image" content={`/public/remoteja1200x628.png`} />
+
+        {/* Twitter */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://remoteja.com/" />
+        <meta
+          property="twitter:title"
+          content="Remoteja | Remote Jobs in Programming, Sales, Marketing, Design and more"
+        />
+        <meta
+          property="twitter:description"
+          content="Say yes to remote working and start hiring the best talent from all corners of the world today. Remoteja has the lastest listings for remote jobs in Programming, Sales, Maketing, Design and more."
+        />
+        <meta
+          property="twitter:image"
+          content={`/public/remoteja1200x628.png`}
+        />
       </Head>
       <ChakraProvider>
         <Component {...pageProps} />
