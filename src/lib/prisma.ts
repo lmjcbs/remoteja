2a8b08245a356bca99b66814da091a765d562c71
lmjcbs <liam@lmjcbs.com>
2021-02-09 17:52:42 +0000

    add generate sitemap hook

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,16 @@
+// @ts-nocheck
 import { PrismaClient } from '@prisma/client'
+let prisma: PrismaClient
 
-const prisma = new PrismaClient()
+// https://www.prisma.io/docs/support/help-articles/400%20-%20nextjs-prisma-client-dev-practices
+// check to use this workaround only in development and not in production
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
 export default prisma
